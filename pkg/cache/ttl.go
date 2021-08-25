11eb7bb1332f78b1463ce26c21ff5a7f00b62901
Akash Sharma <akashsharma@live.in>
2021-08-25 19:57:39 +0530

    remove map for storing expired keys

--- pkg/cache/ttl.go
+++ pkg/cache/ttl.go
@@ -2,20 +2,15 @@ package cache
 
 import (
 	"log"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
 const (
-	maxExpiredBuffer = 64
 	gcDuration       = time.Minute * 1
 )
 
 type ttl struct {
 	cache        *inMemoryCache
-	expiredCount uint32
-	expiredMap   sync.Map
 	tick         *time.Ticker
 	shutdown     chan struct{}
 }
@@ -23,16 +18,8 @@ type ttl struct {
 func newTtl(cache *inMemoryCache) *ttl {
 	return &ttl{
 		cache:      cache,
-		expiredMap: sync.Map{},
 		tick:       time.NewTicker(gcDuration),
 		shutdown:   make(chan struct{}, 1),
-	}
-}
-
-func (t *ttl) addExpiredKey(key string) {
-	_, exists := t.expiredMap.LoadOrStore(key, nil)
-	if !exists {
-		atomic.AddUint32(&t.expiredCount, 1)
 	}
 }
 
@@ -46,28 +33,17 @@ func (t *ttl) startCleanUpJob() {
 				log.Println("cleanup job shutdown")
 				return
 			case <-t.tick.C:
-				expiredLen := t.expiredCount
-				if expiredLen > maxExpiredBuffer/2 {
-					var keys []string
-					t.expiredMap.Range(func(key, _ interface{}) bool {
-						keys = append(keys, key.(string))
-						t.expiredMap.Delete(key)
-						return true
-					})
-
-					t.cache.deleteKeys(keys...)
-					atomic.AddUint32(&t.expiredCount, -uint32(len(keys)))
-				}
-
+				var keys []string
 				for _, bucket := range t.cache.buckets {
-					bucket.Lock()
+					bucket.RLock()
 					for k, v := range bucket.items {
 						if v.isExpired() {
-							t.addExpiredKey(k)
+							keys = append(keys, k)
 						}
 					}
-					bucket.Unlock()
+					bucket.RUnlock()
 				}
+				t.cache.deleteKeys(keys...)
 			}
 		}
 	}()
